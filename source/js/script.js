e02472213491a80b7313923f561bd5797e059eaf
karn-karn <71032527+karn-karn@users.noreply.github.com>
2021-09-16 19:35:50 +0300

    Модальное окно (выбор размера)

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,28 @@
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+const cartSent = document.querySelectorAll(".button-order");
+const cartPopup = document.querySelector(".page__modal");
+const cartClose = cartPopup.querySelector(".modal__button");
+
+for (var i = 0; i < cartSent.length; i++) {
+  (function(index) {
+    cartSent[index].addEventListener("click", function(evt) {
+      evt.preventDefault();
+      cartPopup.classList.add("js-modal-show");
+  });
+
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("js-modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("js-modal-show")) {
+      evt.preventDefault();
+      cartPopup.classList.remove("js-modal-show");
+    }
+  }
+});
